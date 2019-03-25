c6342217878b76ddd71798f4640027cfb120c56e
Unknown <fiona-ea@yandex.by>
2019-03-25 01:21:41 +0100

    commit from 25/03/2019

---
+++ bubbleSort.java
@@ -0,0 +1,47 @@
+//Java implementation of non-optimised bubbleSort
+//average case and worst case: quadratic
+//best case: n
+
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class bubbleSort{
+	public static void main(String args[]) {
+		int size = 10;
+		int ar[]=new int[size];
+		Random rand = new Random();
+		
+		
+		for(int i=0;i<size;i++) {
+			ar[i]=rand.nextInt(100);
+		}
+		
+		System.out.println(Arrays.toString(ar));
+		
+		int dummy;
+		boolean changed = true;
+		int iteration=0;
+				
+		while (changed) {
+			changed=false;
+			iteration+=1;
+			for (int i=0;i<(ar.length-1); i++) {
+				if (ar[i+1]<ar[i]) {
+					dummy=ar[i];
+					ar[i]=ar[i+1];
+					ar[i+1]=dummy;
+					changed=true;
+					
+				}
+				else {
+					continue;
+				}
+			}
+		}
+		
+		System.out.println("needed iterations: " + iteration);
+		System.out.println(Arrays.toString(ar));
+	}
+	
+}
